a34729d38a595d9a8b3c63a873d5d0df6c869cca
Bianca Jemison <bjemison@shipt.com>
2021-10-21 18:53:14 -0500

    add handlers.go

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,1 +1,30 @@
 package handlers
+
+import (
+	""
+	"github.com/gpark1005/FlashCardsTeamThree/entities"
+	"github.com/gpark1005/FlashCardsTeamThree/repo"
+	"net/http"
+	//"github.com/gorilla/mux"
+)
+
+type Service interface{
+	CreateFlashcard (f entities.Flashcards) (*repo.Database, error)
+}
+
+type FlashcardHandler struct {
+	Svc Service
+}
+
+func NewFlashcardHandler(s Service) FlashcardHandler {
+	return FlashcardHandler{
+		s,
+	}
+
+}
+
+func (fh FlashcardHandler) PostFlashCardHandler (w http.ResponseWriter, r *http.Request){
+	//vars := mux.Vars(r)
+	//getVar := vars["type"]
+
+}

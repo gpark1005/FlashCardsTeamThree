3ac11de6e297e2359cc97b661153402b1994df28
ghilliard <ghilliard@shipt.com>
2021-10-20 20:13:50 -0500

    add entities with repo struct

--- entities/entities.go
+++ entities/entities.go
@@ -1,9 +1,49 @@
 package entities
 
+type Question struct {
+	Questions []string
+}
+
+type Choices struct {
+	Choices map[int]string
+}
+
+type Answers struct {
+	Answers map[string]int
+}
+
 type Matching struct {
 	Type string
 	Category string
-	Question []string
-	Choices map[int]string
-	Answers map[string]int
+	Question Question
+	Choices Choices
+	Answers Answers
+}
+
+type InfoOnly struct {
+	Id string
+	Type string
+	Information string
+}
+
+type QAndA struct {
+	Type string
+	Category string
+	Question string
+	Answer string
+}
+
+type TorF struct {
+	Type string
+	Category string
+	Question string
+	Answer string
+}
+
+type MultipleChoice struct {
+	Type string
+	Category string
+	Question string
+	Choices Choices
+	Answer string
 }
--- repo/repo.go
+++ repo/repo.go
@@ -1,3 +1,15 @@
 package repo
 
+import (
+	"github.com/gpark1005/FlashCardsTeamThree/entities"
 
+)
+
+type FlashCards struct {
+	Matching entities.Matching
+	InfoOnly entities.InfoOnly
+	QAndA entities.QAndA
+	TOrF entities.TorF
+	MultipleChoice entities.MultipleChoice
+
+}
